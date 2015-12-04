7662433a554450618edee30ab3ab6dfd95b6d90b
Wopsie <joaghim.beltman@gmail.com>
2015-12-04 13:56:39 +0100

    started OOP

---
+++ AIM3D/Assets/Scripts/Plane.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plane : MonoBehaviour {
+
+    private float speed = 90f;
+
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        speed -= transform.forward.y * Time.deltaTime * 50f;
+
+        //minimum & maximum speed
+        if (speed < 45f)
+        {
+            speed = 45f;
+        }
+        else if (speed > 160f)
+        {
+            speed = 160f;
+        }
+
+        transform.Rotate(Input.GetAxis("Vertical") * 1.5f, 0.0f, -Input.GetAxis("Horizontal") * 1.8f);
+
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if (terrainHeightWhereWeAre > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeightWhereWeAre, transform.position.z);
+        }
+    }
+}
---
+++ AIM3D/Assets/Scripts/TrackerCam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackerCam : MonoBehaviour {
+
+    [SerializeField]    private Transform player;
+	
+	void FixedUpdate () 
+    {
+        //track player with camera
+        Vector3 moveCamTo = player.position - player.forward * 10f + Vector3.up * 5f;
+        float bias = 0.90f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1f - bias);
+        Camera.main.transform.LookAt(player.position + player.forward * 60f);
+	}
+}
