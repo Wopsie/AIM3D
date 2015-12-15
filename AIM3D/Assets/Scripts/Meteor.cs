4f3cd5d08f8c77f3ec6e48d977ad206a574204c2
Tim <tbajmat@gmail.com>
2015-12-15 15:55:06 +0100

    Skybox

--- AIM3D/Assets/Scripts/Meteor.cs
+++ AIM3D/Assets/Scripts/Meteor.cs
@@ -8,6 +8,8 @@ public class Meteor : MonoBehaviour
 	void Update ()
 	{
 		transform.Rotate(Vector3.up, speed * Time.deltaTime);
-		transform.Rotate(Vector3.left, speed * Time.deltaTime);
+		transform.Rotate(Vector3.right, speed * Time.deltaTime);
+
+
 	}
 }
