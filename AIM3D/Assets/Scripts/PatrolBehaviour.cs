0725c1bb7c190a211fa6e205748335f83da50128
Tim <tbajmat@gmail.com>
2016-01-11 11:09:49 +0100

    Kleine tweaks

--- AIM3D/Assets/Scripts/PatrolBehaviour.cs
+++ AIM3D/Assets/Scripts/PatrolBehaviour.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class PatrolBehaviour : MonoBehaviour {
 
+	public enum State
+	{
+		PATROL,
+		CHASE
+	}
+	public State state;
+
+	[SerializeField] private bool alive;
+
 	public Transform[] waypoints;
 	public Transform movingTo;
 	public float speed;
@@ -13,20 +22,38 @@ public class PatrolBehaviour : MonoBehaviour {
 
 	void Start()
 	{
+		StartCoroutine("FSM");
+		Patrol();
+	}
+
+	IEnumerator FSM()
+	{
+		while(alive)
+		{
+			switch(state)
+			{
+			case State.PATROL:
+				Patrol();
+				break;
+			case State.CHASE:
+				Chase();
+				break;
+			}
+			yield return null;
+		}
+	}
+
+	void Patrol()
+	{
 		movingTo = waypoints[0];
 		waypointIndex = 0;
-
+		
 		if(Vector3.Distance(transform.position, waypoints[0].position) < 1)
 		{
 			movingTo = waypoints[1];
 			waypointIndex = 1;
 		}
 
-
-	}
-
-	void FixedUpdate()
-	{
 		if(Vector3.Distance(transform.position, movingTo.position) < 1)
 		{
 			if(waypointIndex == (waypoints.Length - 1))
@@ -41,22 +68,28 @@ public class PatrolBehaviour : MonoBehaviour {
 			}
 		}
 
+		transform.position = Vector3.MoveTowards(transform.position, movingTo.position, Time.deltaTime * speed);
+
+	}
+
+	void Chase()
+	{
+		Debug.Log("CHASE!");
+	}
+
+	void Update()
+	{
 		Vector3 desiredStep	= movingTo.position - GetComponent<Rigidbody>().position;
 		desiredStep.Normalize();
 		Vector3 desiredVelocity	= desiredStep	*	maxSpeed;
 		Vector3 steeringForce = desiredVelocity - GetComponent<Rigidbody>().velocity;
 		GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity + steeringForce / mass;
 
-		transform.position = Vector3.MoveTowards(transform.position, movingTo.position, Time.deltaTime * speed);
 
 		Vector3 targetDir = movingTo.position - transform.position;
 		float step = speed * Time.deltaTime;
 		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-		//.DrawRay(transform.position, newDir, Color.red);
 		transform.rotation = Quaternion.LookRotation(newDir);
-
-		//Vector3 relativePos = movingTo.position - transform.position;
-		//Quaternion rotation = Quaternion.LookRotation(desiredStep);
-		//transform.rotation = rotation;
 	}
 }
+
