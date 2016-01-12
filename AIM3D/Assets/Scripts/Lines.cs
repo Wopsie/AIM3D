5ba4f4845d33fa53cd6f596bb7b67720309165ab
Wopsie <joaghim.beltman@gmail.com>
2016-01-12 11:53:11 +0100

    added respawn feature & turret aim bullshit

---
+++ AIM3D/Assets/Scripts/Lines.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lines : MonoBehaviour
+{
+    public GameObject makeLine(Vector3 to, Color kleur)
+    {
+        var tempobject = new GameObject("line");
+        //tempobject.transform.parent = this.transform;
+        var templine = tempobject.AddComponent<LineRenderer>();
+        templine.SetWidth(10f, 10f);
+        templine.material = new Material(Shader.Find("Particles/Additive"));
+        templine.SetColors(kleur, kleur);
+        templine.SetPosition(0, this.transform.position);
+        templine.SetPosition(1, to);
+        return tempobject;
+    }
+
+    public void drawLineTo(GameObject line, Vector3 from, Vector3 to)
+    {
+        line.GetComponent<LineRenderer>().SetPosition(0, from);
+        line.GetComponent<LineRenderer>().SetPosition(1, to);
+    }
+}
---
+++ AIM3D/Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnScript : MonoBehaviour {
+
+    [SerializeField]    private GameObject player;
+    private GameObject deathCamera;
+    private Camera deathCam;
+    private int lives = 3;
+    private PlayerHealth pHealth;
+    private GameObject tracker;
+    private Camera trackerCam;
+    private TrackerCam trackerScript;
+
+    void Start()
+    {
+        tracker = GameObject.FindWithTag("MainCamera");
+        trackerCam = tracker.GetComponent<Camera>();
+        trackerScript = tracker.GetComponent<TrackerCam>();
+
+        deathCamera = GameObject.FindWithTag(Tags.deathCam);
+        deathCam = deathCamera.GetComponent<Camera>();
+        pHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    public void Respawn()
+    {
+        //check for sufficient lives
+        
+        if(lives > 0)
+        {
+            //Debug.Log("respawning... " + lives);
+            //add new player on respawn location & reenable main camera & disable deathcamera
+            Instantiate(player, transform.position, transform.rotation);
+            deathCam.enabled = false;
+            Camera.main.enabled = true;
+            trackerScript.RefindPlayer();
+            lives--;
+            Debug.Log(lives + " lives left");
+        }
+        else
+        {
+            //deathsceen
+            Debug.Log("Mission Failed");
+        }
+        
+    }
+}
